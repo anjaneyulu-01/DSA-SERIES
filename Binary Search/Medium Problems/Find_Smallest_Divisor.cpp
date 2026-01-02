cd582802ad40327cd86c39f977159e1448479b65
ANJANEYULU <anjaneyulu.133107@marwadiuniversity.ac.in>
2026-01-02 13:01:55 +0530

    Solution for Find the smallest divisor

---
+++ Binary Search/Medium Problems/Find_Smallest_Divisor.cpp
@@ -0,0 +1,62 @@
+/*
+Find the smallest divisor
+
+Given an array of integers nums and an integer limit as the threshold value, find the smallest positive integer divisor such that upon dividing all the elements of the array by this divisor, the sum of the division results is less than or equal to the threshold value.
+
+After dividing each element by the chosen divisor, take the ceiling of the result (i.e., round up to the next whole number).
+
+
+Example 1
+
+Input: nums = [1, 2, 3, 4, 5], limit = 8
+
+Output: 3
+
+Explanation: We can get a sum of 15(1 + 2 + 3 + 4 + 5) if we choose 1 as a divisor. 
+
+The sum is 9(1 + 1 + 2 + 2 + 3) if we choose 2 as a divisor. Upon dividing all the elements of the array by 3, we get 1,1,1,2,2 respectively. Now, their sum is equal to 7 <= 8 i.e. the threshold value. So, 3 is the minimum possible answer.
+
+Example 2
+
+Input: nums = [8,4,2,3], limit = 10
+
+Output: 2
+
+Explanation: If we choose 1, we get 17 as the sum. If we choose 2, we get 9 (4+2+1+2) <= 10 as the answer. So, 2 is the answer.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+int smallestDivisor(vector<int>& nums, int threshold) {
+  int low = 1;
+  int high = *max_element(nums.begin(), nums.end());
+  int ans = high;
+
+  while (low <= high) {
+      int mid=low+(high - low)/2;
+      long long sum=0;
+    for (int x:nums) {
+          sum+=(x+mid-1)/mid;
+      }
+    if (sum<=threshold) {
+          ans=mid;
+          high=mid-1; 
+      } else {
+          low=mid + 1;   
+      }
+  }
+  return ans;
+}
+};
+
+
+int main(){
+  vector<int>nums={1, 2, 3, 4, 5};
+  int limit=8;
+  Solution obj;
+  cout<<obj.smallestDivisor(nums,limit);
+  return 0;
+}
