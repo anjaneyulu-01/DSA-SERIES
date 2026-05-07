cb2fa6ea8b6ce9f0dfefa00f4adc8890bfd2cec5
ANJANEYULU <anjaneyulu.133107@marwadiuniversity.ac.in>
2026-05-07 20:26:31 +0530

    Solution for Single-3 number

--- Bit Manipulation/Medium/Single-Number-III.cpp
+++ Bit Manipulation/Medium/Single-Number-III.cpp
@@ -1,12 +1,7 @@
 /*
 Single Number - III
 Given an array nums of length n, every integer in the array appears twice except for two integers. Identify and return the two integers that appear only once in the array. Return the two numbers in ascending order.
-
-
-
 For example, if nums = [1, 2, 1, 3, 5, 2], the correct answer is [3, 5], not [5, 3].
-
-
 Example 1
 
 Input : nums = [1, 2, 1, 3, 5, 2]
